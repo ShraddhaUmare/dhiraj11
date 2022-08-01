2552d363ba680c386c3745a5f3c68a581ee0c6ab
SKY <SKY@SKY-PC>
2022-08-01 17:28:14 +0530

    new by system

---
+++ 01-08-2022_Git-demo/src/cjc/demo.java
@@ -0,0 +1,15 @@
+package cjc;
+
+public class demo 
+{
+	
+
+		public static void main(String[] args) 
+		{
+			System.out.println("this is git demo");
+
+			System.out.println("this is git demo2");
+		}
+
+	}
+
